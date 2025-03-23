66877b3f5a64c0de85080b54d4cc3332eec78687
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-23 12:25:13 -0500

    add gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import viteReact from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact()],
+  base: '/epm-react-challenge', //github pages
   test: {
     globals: true,
     environment: "jsdom",
